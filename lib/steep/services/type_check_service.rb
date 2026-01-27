ea3fbb6087a7ae9683d610be7f722ab8056be81e
MATSUBARA Nobutada <nobutada0507@gmail.com>
2026-01-27 17:52:24 +0900

    debug

--- lib/steep/services/type_check_service.rb
+++ lib/steep/services/type_check_service.rb
@@ -233,6 +233,8 @@ module Steep
           Steep.measure "typecheck" do
             signature_service = signature_services.fetch(target.name)
             subtyping = signature_service.current_subtyping
+            p [path, subtyping]
+            p [path, signature_service.status.diagnostics] if subtyping.nil?
 
             if subtyping
               text = source_files.fetch(path).content
